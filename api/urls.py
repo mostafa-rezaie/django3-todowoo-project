373d596f1db0af10c9df5b57655cdd73767053f1
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-06-18 16:18:34 +0430

    Signup and Login TokenAuthentication

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import TodoCompletedList, TodoCreateList, TodoUpdateDestroyRetrieve, TodoComplete
+from .views import signup, getTheToken,login
 
 urlpatterns = [
     path('todos', TodoCreateList.as_view()),
     path('todos/<int:pk>', TodoUpdateDestroyRetrieve.as_view()),
     path('todos/<int:pk>/complete', TodoComplete.as_view()),
     path('todos/completed', TodoCompletedList.as_view()),
+
+    path('sign-up', signup),
+    path('test', getTheToken.as_view()),
+    path('login',login)
 ]
